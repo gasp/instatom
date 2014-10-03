646109382860226ae3accac5b2d7bb46212063a6
gaspard <gaspard@gmail.com>
2014-10-03 14:29:38 +0200

    adding twig service provider

--- web/index.php
+++ web/index.php
@@ -1,11 +1,32 @@
-<?php 
-require_once __DIR__.'/../vendor/autoload.php';
+<?php
+require __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 $app['debug'] = true;
 
-$app->get('{user}', function ($user) use ($app) {
-	return 'hello '.$app->escape($user);
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+	'twig.path' => __DIR__.'/../views',
+));
+
+$app->get('/u/{user}', function ($user) use ($app) {
+	return $app['twig']->render('atom.twig', array(
+		'user' => $user,
+	));
+});
+
+$app->error(function (\Exception $e, $code) use ($app) {
+	if ($app['debug']) {
+		return;
+	}
+	switch ($code) {
+		case 404:
+			$message = 'The requested page could not be found.';
+			break;
+		default:
+			$message = 'We are sorry, but something went terribly wrong.';
+	}
+	return new Response($message, $code);
 });
 
 $app->run();
+
